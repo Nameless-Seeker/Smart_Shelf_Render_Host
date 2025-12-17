c1c938faee1cdb9de17201dd99ee215c0ec0d933
Srijan Sarkar <srijanarkar.coding@gmail.com>
2025-12-17 23:55:24 +0530

    Test 1

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"info": "My name is Srijan Sarkar"}
+
+@app.get("/health")
+def health():
+    return {"messege":"OK"}
